b840ae3135441937cfefed657876c6d0dbc04f01
avearmin <arminradoncic1@gmail.com>
2024-02-14 19:05:09 -0500

    Add tests for count.Lines

--- internal/count/count_test.go
+++ internal/count/count_test.go
@@ -25,3 +25,27 @@ func TestBytes(t *testing.T) {
 		})
 	}
 }
+
+func TestLines(t *testing.T) {
+	tests := map[string]struct {
+		input io.Reader
+		want  int
+	}{
+		"3 lines": {
+			strings.NewReader("Hello world!\nHello universe!\nHello multiverse!"),
+			3,
+		},
+		"1 lines": {
+			strings.NewReader(""),
+			1,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			if got, _ := Lines(test.input); test.want != got {
+				t.Errorf("FAIL want: %2d got: %2d", test.want, got)
+			}
+		})
+	}
+}
